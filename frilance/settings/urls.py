e6d185a48b9958c87b6b57fbafc8a652703213b9
po5e1don22 <153468702+po5e1don22@users.noreply.github.com>
2024-03-20 00:57:52 +0300

    feat: Add user password change functionality
    - Implemented a feature allowing users to change their password

--- frilance/settings/urls.py
+++ frilance/settings/urls.py
@@ -6,4 +6,6 @@ app_name = "settings"
 urlpatterns = [
     path("", views.settings, name = 'settings'),
     path('settings/profile_update/', views.profile_update, name='profile_update'),
+    path('settings/change_password/', views.change_password, name='change_password'),
+
 ]
--- frilance/settings/views.py
+++ frilance/settings/views.py
@@ -1,4 +1,6 @@
-
+from urllib import request
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -11,23 +13,30 @@ from django.conf import settings as django_settings
 def settings(request):
     return render(request, 'settings/setting.html')
 
-
+@login_required
 def profile_update(request):
     if request.method == 'POST':
         username = request.POST.get('profile-name')
-        email = request.POST.get('profile-email')
+
+        email = request.POST.get('profile-email')\
+
         image = request.FILES.get('profile-image')
 
         if username:
             request.user.username = username
+
         if email:
             request.user.email = email
+
         if image:
             old_image_path = None
+
             if request.user.account.image and os.path.isfile(os.path.join(django_settings.MEDIA_ROOT, request.user.account.image.path)):
                 old_image_path = os.path.join(django_settings.MEDIA_ROOT, request.user.account.image.path)
+
             request.user.account.image = image
             request.user.account.save()
+
             if old_image_path:
                 os.remove(old_image_path)
 
@@ -36,3 +45,20 @@ def profile_update(request):
         return redirect('settings:settings') 
 
     return render(request,'settings/setting.html')
+
+@login_required()
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Ваш пароль был успешно обновлен!')
+            return redirect('settings:settings')
+        else:
+            messages.error(request, 'Пожалуйста, исправьте ошибку.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'settings/setting.html', {
+        'form': form
+    })
